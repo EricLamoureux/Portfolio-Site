3b2245e857943255513b6be54f22a3df161a29a2
EricLamoureux <elamoure@my.centennialcollege.ca>
2024-09-29 17:01:01 -0400

    About and Home Screens
    Improvements to About and Home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,13 @@
 
 const About = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-      <img src="/images/Eric.png" alt="Logo" style={{ width: "200px", borderRadius: "50%" }} />
-      <p>Hi, I'm Eric! I'm a passionate developer with expertise in game development, 3D Modelling and Animation.</p>
-      <a href="/images/Eric_Lamoureux_Resume.pdf" download>Download My Resume</a>
+    <div className="about-container">
+      <img src="/images/Eric.png" alt="Logo" style={{ width: "150px", borderRadius: "50%" }} />
+      <div className="about-text">
+        <h1>About Me</h1>
+        <p>Hi, I'm Eric! I'm a passionate developer with expertise in game development, 3D Modelling, and Animation.
+        <a href="/images/Eric_Lamoureux_Resume.pdf" download> Download My Resume</a> </p>
+      </div>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,15 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-      <div>
+      <div className="home-container">
         <h1>Welcome to My Portfolio</h1>
         <p>Mission Statement: I am dedicated to creating impactful digital solutions through my skills in development and design.</p>
-        <button><Link to="/about">About Me</Link></button>
-        <button><Link to="/projects">Projects</Link></button> <br/>
-        <button><Link to="/services">Services</Link></button>
-        <button><Link to="/contact">Contact Me</Link></button>
+        <div className="button-container">
+          <button><Link to="/about">About Me</Link></button>
+          <button><Link to="/projects">Projects</Link></button>
+          <button><Link to="/services">Services</Link></button>
+          <button><Link to="/contact">Contact Me</Link></button>
+        </div>
       </div>
     );
 };
